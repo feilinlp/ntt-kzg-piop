c739c19dbaaf201467d155d974955a848b79df24
feilin <feilin.liangga@gmail.com>
2025-06-16 18:34:57 +0800

    Update to PIOP process

--- src/sumcheck/sumcheck.cpp
+++ src/sumcheck/sumcheck.cpp
@@ -18,36 +18,38 @@ bool sumCheck(KZG::PublicKey pk, vector<Fr> q, Fr w, size_t l, Fr s) {
         curr *= w;
     }
 
-    // Prover's evaluation
     // Vanishing Polynomial zh(x) = x^l - 1
     vector<Fr> zh(l+1, 0);
     zh[0] = -1;
     zh[l] = 1;
-
+    
+    // Prover's evaluation
     // Since zh(x) = x^l - 1, polynomialDivision is O(D)F
     vector<Fr> temp = q;
     temp[0] -= s / l;
     vector<Fr> f = polynomialDivision(temp, zh);
     
-    KZG::Commitment comm = commit(pk, f); // O(D)G
+    KZG::Commitment comm_f = commit(pk, f); // O(D)G
+    KZG::Commitment comm_q = commit(pk, q); // O(D)G
 
-    // Prover sends s and comm to Verifier
+    // Prover sends comm_f and comm_q to Verifier
     // Verifier sends random challenge r to Prover
     Fr r;
     r.setByCSPRNG();
 
-    // Prover's evaluation
-    Fr zr = evaluatePolynomial(zh, r); // O(D)F, same for fr and qr
-    Fr fr = evaluatePolynomial(f, r); 
-    Fr qr = evaluatePolynomial(q, r); 
+    // Prover evaluates the values to f(r) and q(r)
+    KZG::Witness witness_f = createWitness(pk, f, r); // Witness to fr --> O(D)G
+    KZG::Witness witness_q = createWitness(pk, q, r); // Witness to qr --> O(D)G
 
-    // What happens after this???
-    KZG::Witness witness = createWitness(pk, f, r); // Witness to fr --> O(D)G
+    // Prover sends to Verifier: witness_f and witness_q
 
-    // Prover sends to Verifier: witness, qr, fr, zr
+    // Public knowledge --> Both Prover and Verifier can evaluate Zh(r) in O(1)F
+    Fr zr;
+    Fr::pow(zr, r, l);
+    zr -= 1;
 
     // Verifier's evaluation
-    // V checks if the commitment and witness open to f(r) --> O(1)G
-    // V also checks that the evaluated qr = fr * zr + s / l --> O(1)F
-    return verifyEval(pk, comm, witness) && qr == fr * zr + s / l;
+    // V checks if the commitment and witness open to f(r) and q(r) --> O(1)G
+    // V also checks that the evaluated qr = fr * zr --> O(1)F
+    return verifyEval(pk, comm_f, witness_f) && verifyEval(pk, comm_q, witness_q) && witness_q.w == witness_f.w * zr + s / l;
 }
